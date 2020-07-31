0dc333275f853a3cafe304d56edeaecb30abebe1
Shelley Brook <sbrook13@gmail.com>
2020-07-31 22:22:57 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,51 @@
 # require modules here
+require 'pry'
 
-def load_library
-  # code goes here
+require "yaml"
+  
+def load_library(file)
+  emoticons = YAML.load_file(file)
+  new_emoticons = {}
+  emoticons.each do |key, values| 
+      new_emoticons[key] = {}
+      new_emoticons[key][:english] = emoticons[key][0]
+      new_emoticons[key][:japanese] = emoticons[key][1]
+  end
+  new_emoticons
 end
 
-def get_japanese_emoticon
-  # code goes here
+
+def get_japanese_emoticon(file, emoticon)
+  emoticons = YAML.load_file(file)
+  new_emoticons = {}
+  emoticons.each do |key, values| 
+      new_emoticons[key] = {}
+      new_emoticons[key][:english] = emoticons[key][0]
+      new_emoticons[key][:japanese] = emoticons[key][1]
+  end
+  new_emoticons.each do |key, hash|
+    # binding.pry
+    if new_emoticons[key][:english] == emoticon
+      return new_emoticons[key][:japanese]
+    else !new_emoticons[key][:english] == emoticon
+      "Sorry, that emoticon was not found"
+    end
+  end  
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(file, emoticon)
+  emoticons = YAML.load_file(file)
+  new_emoticons = {}
+  emoticons.each do |key, values| 
+      new_emoticons[key] = {}
+      new_emoticons[key][:english] = emoticons[key][0]
+      new_emoticons[key][:japanese] = emoticons[key][1]
+  end
+  new_emoticons.each do |key, hash|
+    if new_emoticons[key][:japanese] == emoticon
+      return new_emoticons[key][:english]
+    else !new_emoticons[key][:japanese] == emoticon
+      "Sorry, that emoticon was not found"
+    end
+  end
 end
